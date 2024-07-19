2a3f0558e0e0d1c32e603ff5a24619d1e3c2c9d5
nareshbabumandula <naresh223@gmail.com>
2024-07-19 07:48:07 +0530

    Commiting the selenium java maven project

---
+++ SeleniumJava072024/src/test/java/com/selenium/scripts/BrowserTest.java
@@ -0,0 +1,9 @@
+package com.selenium.scripts;
+
+public class BrowserTest {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world welcome to Selenium training..!");
+	}
+
+}
