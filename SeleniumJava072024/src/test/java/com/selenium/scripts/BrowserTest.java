280d471d69997073f60c3032de3c74e78a31fd06
nareshbabumandula <naresh223@gmail.com>
2024-07-23 07:58:48 +0530

    23-Jul-2024: Naresh: Selenium code to work with different browsing launching and close

--- SeleniumJava072024/src/test/java/com/selenium/scripts/BrowserTest.java
+++ SeleniumJava072024/src/test/java/com/selenium/scripts/BrowserTest.java
@@ -1,9 +1,41 @@
 package com.selenium.scripts;
 
-public class BrowserTest {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
-	public static void main(String[] args) {
-		System.out.println("Hello world welcome to Selenium training..!");
+public class BrowserTest {
+	WebDriver driver;
+	
+	public void accessSite(String browser) throws InterruptedException {
+		switch (browser.toLowerCase().trim()) {
+		case "chrome":
+			driver = new ChromeDriver();
+			break;
+		case "edge":
+			driver = new EdgeDriver();
+			break;
+		case "firefox":
+			driver = new FirefoxDriver();
+			break;
+		case "safari":
+			driver = new SafariDriver();
+			break;
+		default:
+			System.out.println("Invalid browser..!");
+			break;
+		}
+		driver.get("https://www.mycontactform.com");
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+		driver.quit();
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		BrowserTest bt = new BrowserTest();
+		bt.accessSite("chrome");
 	}
 
 }
