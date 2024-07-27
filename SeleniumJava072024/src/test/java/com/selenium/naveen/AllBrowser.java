be4c412f7702d8a63839134079739b94a6059eb2
naveen198961 <tadakapallynaveen53@gmail.com>
2024-07-27 15:53:11 +0530

    Update AllBrowser.java

--- SeleniumJava072024/src/test/java/com/selenium/naveen/AllBrowser.java
+++ SeleniumJava072024/src/test/java/com/selenium/naveen/AllBrowser.java
@@ -2,17 +2,12 @@ package com.selenium.naveen;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-
 public class AllBrowser {
-
 	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver= new EdgeDriver();
 		driver.get("https://wwww.eyeglassworld.com");
 		driver.manage().window().maximize();
 		Thread.sleep(2000);
 		driver.quit();
-		
-
 	}
-
 }
