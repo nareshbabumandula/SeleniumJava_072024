6b8c383112e0034ca257c4b11a86bb88b7a93c52
Sapth <Sapth@Giri>
2024-08-06 17:18:58 +0530

    Different module names are printed on html file from website mycontactform.com

---
+++ SeleniumJava072024/src/test/java/com/selenium/scripts/MCFSampleFormsAllNames.java
@@ -0,0 +1,58 @@
+package com.selenium.scripts;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+public class MCFSampleFormsAllNames {
+	WebDriver driver;
+	static ExtentTest test;
+	static ExtentReports report;
+	@Test
+	public void xpathaxesExamples() throws InterruptedException {
+		try {
+			driver.findElement(By.linkText("Sample Forms")).click();
+			List<WebElement> tabs = driver.findElements(By.xpath("//div[@id='left_col_top']//following-sibling::*"));
+			System.out.println("No of tabs displayed are : " + tabs.size());
+			test.log(LogStatus.PASS, "No of tabs displayed in MCF page are : " + tabs.size());
+			
+			tabs.forEach(tab->System.out.println(tab.getText()));
+			
+			for (WebElement tab : tabs) {
+				System.out.println("Tab name is : " + tab.getText());
+				test.log(LogStatus.PASS, "'" + tab.getText() +  "' tab is displayed");
+			}
+			
+			Thread.sleep(1000);
+			test.log(LogStatus.PASS, "Successfully retrived all the tab names");
+			
+		} catch(Exception e) {
+			test.log(LogStatus.FAIL, "Test Failed" + e.getMessage());
+		}
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+		driver = new ChromeDriver();
+		driver.get("https://mycontactform.com");
+		driver.manage().window().maximize();
+		report = new ExtentReports("./ExtentReport_MCFSampleFormsLinkNames.html");
+		test = report.startTest("TC001");
+	}
+
+	@AfterClass
+	public void afterClass() {
+		report.endTest(test);
+		report.flush();
+		driver.quit();
+	}
+}
+
